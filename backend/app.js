c8a84fa04bd645b0199405eb03d4fc0142fe292c
Josue Barrios R <josue.barrios@gmail.com>
2019-09-28 19:09:25 -0500

    Configuracion de la base de datos y el backend

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+// requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+
+// inicializacion
+var app = express();
+
+
+
+// conexion a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    if (err) throw err;
+    console.log("Base de datos: \x1b[32m%s\x1b[0m","online");
+});
+
+// rutas
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'peticion ok'
+    });
+});
+
+
+
+// run server
+app.listen(3000, () => {
+    console.log("Express server puerto 3000: \x1b[32m%s\x1b[0m","online");
+})
